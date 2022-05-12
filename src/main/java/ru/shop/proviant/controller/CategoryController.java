f636ae4c26ceda048398b3554146dbe6843a24dd
R1con <igor.prilutzkij@yandex.ru>
2022-05-12 10:40:39 +0300

    Создан класс ProductController для отправки из БД данных

--- src/main/java/ru/shop/proviant/controller/CategoryController.java
+++ src/main/java/ru/shop/proviant/controller/CategoryController.java
@@ -1,25 +1,25 @@
 package ru.shop.proviant.controller;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.shop.proviant.repository.CategoryRepository;
 
 @RestController
-@RequestMapping("/category")
+@AllArgsConstructor
 public class CategoryController {
 
     private final CategoryRepository categoryRepository;
 
-    public CategoryController(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
-    @GetMapping
+    @CrossOrigin
+    @GetMapping(value = "/category")
     public ResponseEntity getAllCategory(){
-        return ResponseEntity.ok(this.categoryRepository.findAll());
+       return ResponseEntity.ok(categoryRepository.findAll());
     }
 
 
