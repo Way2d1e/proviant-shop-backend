5d30454be853c488dcf8feeeed06bdbc5092ce7c
halpears <texatema@gmail.com>
2022-05-18 23:01:42 +0300

    Все сделать

--- src/main/java/ru/shop/proviant/repository/OrderItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.shop.proviant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.shop.proviant.model.OrderItems;
-
-public interface OrderItemsRepository extends JpaRepository<OrderItems, Long> {
-}
