6200c2b5495cc9ce4fe64c8f1ceabc6eb7b18118
R1con <igor.prilutzkij@yandex.ru>
2022-05-19 11:50:03 +0300

    rename mappers to mapper

--- src/main/java/ru/shop/proviant/mapper/CategoryMapper.java
+++ src/main/java/ru/shop/proviant/mapper/CategoryMapper.java
@@ -1,8 +1,8 @@
-package ru.shop.proviant.mappers;
+package ru.shop.proviant.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import ru.shop.proviant.model.dto.CategoryDto;
+import ru.shop.proviant.model.entity.dto.CategoryDto;
 import ru.shop.proviant.model.entity.Category;
 
 import java.util.List;
--- src/main/java/ru/shop/proviant/mapper/ProductMapper.java
+++ src/main/java/ru/shop/proviant/mapper/ProductMapper.java
@@ -1,9 +1,8 @@
-package ru.shop.proviant.mappers;
+package ru.shop.proviant.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import ru.shop.proviant.model.dto.ProductDto;
+import ru.shop.proviant.model.entity.dto.ProductDto;
 import ru.shop.proviant.model.entity.Product;
 
 import java.util.List;
@@ -13,6 +12,8 @@ public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
     ProductDto toDto(Product product);
+
+    Product dtoToEntity(ProductDto productDto);
 
     List<ProductDto> listDto(List<Product> products);
 }
